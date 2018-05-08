d1b4d45a78ebd7de47c269d05a6ddbfe5b69335d
giovannicandeo <giovanni.candeo.1@gmail.com>
2018-05-08 13:57:52 +0200

    updated User.java to match database, added also a "faster" costructor

--- src/main/java/project/resource/User.java
+++ src/main/java/project/resource/User.java
@@ -16,123 +16,155 @@ public class User extends Resource
 	/**
 	 * The Username of the user
 	 */
-	private final String username;
-	private final String description;
-	private final String name;
-	private final String surname;
-	private final String email;
-	private final Date registrationDate;
-	private final String profilePic;
-	private final Date birthday;
+    private final String email;
+    private final String name;
+    private final String surname;
+    private final String username;
+    private final String photoProfile;
+    private final String password;
+    private final Date birthday;
+    private final Date registrationDate;
+    private final String description;
 
-	/**
-	 * Creates a new employee
-	 * 
-	 * @param username
-	 *            the username of the user
-	 * @param description
-	 *            the descriptiom of the user
-	 * @param name
-	 *            the name of the user
-	 * @param surname
-	 *            the surname of the user
-	 * @param email
-	 *            the email of the user
-	 * @param registrationDate
-	 *            the registrationDate of the user
-	 * @param profilePic
-	 *            the directory of the user's profile picture
-	 */
-	public User(final String username, final String description, final String name, final String surname, final String email, final Date registrationDate, final String profilePic, final Date birthday)
-	{
-		this.username = username;
-		this.description = description;
-		this.name = name;
-		this.surname = surname;
-		this.email = email;
-		this.registrationDate = registrationDate;
-		this.profilePic = profilePic;
-		this.birthday = birthday;
-	}
+    /**
+     * Creates a new user
+     * @param email
+     *              the email of the user
+     * @param name
+     *              the name of the user
+     * @param surname
+     *              the surname of the user
+     * @param username
+     *              the username of the user
+     * @param photoProfile
+     *              the directory of the user's profile picture
+     * @param password
+     *              the password of the user
+     * @param registrationDate
+     *              the registrationDate of the user
+     * @param birthday
+     *              the birthday of the user
+     * @param description
+     *              the descriptiom of the user
+     */
+    public User(final String email, final String name, final String surname, final String username,
+                final String photoProfile, final String password, final Date registrationDate,
+                final Date birthday, final String description){
+        this.email = email;
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.photoProfile = photoProfile;
+        this.password = password;
+        this.registrationDate = registrationDate;
+        this.birthday = birthday;
+        this.description = description;
+    }
 
-	/**
-	 * Returns the username of the user.
-	 * 
-	 * @return the username of the user.
-	 */
-	public final String getUsername()
-	{
-		return username;
-	}
 
-	/**
-	 * Returns the description of the user.
-	 * 
-	 * @return the description of the user.
-	 */
-	public final String getDescription()
-	{
-		return description;
-	}
+    /**
+     * Creates a new user: name, surname, photoProfile, birthday and description are set to null
+     * @param email
+     *              the email of the user
+     * @param username
+     *              the username of the user
+     * @param password
+     *              the password of the user
+     * @param registrationDate
+     *              the registration date of the user
+     */
+    public User(final String email, final String username, final String password, final Date registrationDate){
 
-	/**
-	 * Returns the name of the user.
-	 * 
-	 * @return the name of the user.
-	 */
-	public final String getName()
-	{
-		return name;
-	}
+        new User(email, null, null, username, null, password, registrationDate,
+                null, null);
+    }
+    /**
+     * Returns the email of the user.
+     *
+     * @return the email of the user.
+     */
+    public final String getEmail()
+    {
+        return email;
+    }
 
-	/**
-	 * Returns the surname of the user.
-	 * 
-	 * @return the surname of the user.
-	 */
-	public final String getSurname()
-	{
-		return surname;
-	}
+    /**
+     * Returns the name of the user.
+     *
+     * @return the name of the user.
+     */
+    public final String getName()
+    {
+        return name;
+    }
 
-	/**
-	 * Returns the email of the user.
-	 * 
-	 * @return the email of the user.
-	 */
-	public final String getEmail()
-	{
-		return email;
-	}
+    /**
+     * Returns the surname of the user.
+     *
+     * @return the surname of the user.
+     */
+    public final String getSurname()
+    {
+        return surname;
+    }
 
-	/**
-	 * Returns the registration date of the user.
-	 * 
-	 * @return the registration date of the user.
-	 */
-	public final Date getRegistrationDate()
-	{
-		return registrationDate;
-	}
-	/**
-	 * Returns the user's profile pic path.
-	 * 
-	 * @return the user's profile pic path.
-	 */
-	public final String getProfilePic()
-	{
-		return profilePic;
-	}
+    /**
+     * Returns the username of the user.
+     *
+     * @return the username of the user.
+     */
+    public final String getUsername()
+    {
+        return username;
+    }
 
-	/**
-	 * Returns the birthday of the user.
-	 * 
-	 * @return the birthday of the user.
-	 */
-	public final Date getBirthday()
-	{
-		return birthday;
-	}
+    /**
+     * Returns the user's profile pic path.
+     *
+     * @return the user's profile pic path.
+     */
+    public final String getPhotoProfile()
+    {
+        return photoProfile;
+    }
+
+    /**
+     * Return the user's password.
+     *
+     * @return the user's password.
+     */
+    public final String getPassword(){
+        return password;
+    }
+    /**
+     * Returns the registration date of the user.
+     *
+     * @return the registration date of the user.
+     */
+    public final Date getRegistrationDate()
+    {
+        return registrationDate;
+    }
+
+    /**
+     * Returns the user's birthday date.
+     *
+     * @return the user's birthday date.
+     */
+    public final Date getBirthday(){
+        return birthday;
+    }
+
+    /**
+     * Returns the description of the user.
+     *
+     * @return the description of the user.
+     */
+    public final String getDescription()
+    {
+        return description;
+    }
+
 
 	@Override
 	public final void toJSON(final OutputStream out) throws IOException {
