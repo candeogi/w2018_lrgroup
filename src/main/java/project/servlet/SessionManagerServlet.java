cb9448c61fd54a0471c13d72e49552fee3ffb19e
giovannicandeo <giovanni.candeo.1@gmail.com>
2018-05-18 11:28:50 +0200

    added some documentation

--- src/main/java/project/servlet/SessionManagerServlet.java
+++ src/main/java/project/servlet/SessionManagerServlet.java
@@ -12,18 +12,31 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServlet;
 
 import java.io.IOException;
-
+/**
+ * Manage the user session.
+ * If user session is not found, redirects to the login form.
+ *
+ * @author Giovanni Candeo
+ * @version 1.00
+ * @since 1.00
+ */
 public class SessionManagerServlet extends AbstractDatabaseServlet{
-    /**
-     * Manage the user session.
-     * If user session is not found, redirects to the login form.
-     *
-     * @author Giovanni Candeo
-     * @version 1.00
-     * @since 1.00
-     */
+
     private HttpSession session;
 
+    /**
+     * Overrides service method called by the servlet container.
+     * If there is a session i allow the servlet to respond to the request; otherwise redirects to the login form.
+     *
+     * @param req
+     *          the HTTP request from the client.
+     * @param res
+     *          the HTTP response from the server.
+     * @throws ServletException
+     *          if an exception has occurred that interferes with the servlet's normal operation
+     * @throws IOException
+     *          if an input or output error occurs while the servlet is handling the HTTP request
+     */
     protected void service(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
 
