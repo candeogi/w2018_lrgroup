fb23f8c3f713ce8d6ef491085af26d5bc1b34deb
apontini <alberto.pontini@gmail.com>
2018-05-25 11:08:34 +0200

    added docs part 1

--- src/main/java/project/database/CreateHaveElement.java
+++ src/main/java/project/database/CreateHaveElement.java
@@ -21,12 +21,28 @@ public class CreateHaveElement {
 
     private final int idAnswer;
 
+
+    /**
+     * Creates a new object for creating a new relation between Question and Answer.
+     *
+     * @param con
+     *            the connection to the database.
+     * @param idQuestion
+     *            the question to be added to the relation.
+     * @param idAnswer
+     *            the answer to be added to the relation.
+     */
     public CreateHaveElement(Connection con, int idQuestion, int idAnswer) {
         this.con = con;
         this.idQuestion = idQuestion;
         this.idAnswer = idAnswer;
     }
 
+    /**
+     * Creates a new relation in the database.
+     * @throws SQLException
+     *             if any error occurs while creating the relation. 
+     */
 
     public void createElement() throws SQLException {
         PreparedStatement pstmt = null;
