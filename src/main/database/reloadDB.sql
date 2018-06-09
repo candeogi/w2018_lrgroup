10dc81a7f6ec4e0e420425d43ab688b08cb1a292
albefrt6 <alberto.forti93@gmail.com>
2018-06-09 11:12:48 +0200

    Added reloadDB

---
+++ src/main/database/reloadDB.sql
@@ -0,0 +1,10 @@
+\connect webapp;
+DROP SCHEMA lr_group CASCADE;
+\i createTableWithSchema.sql
+\i user-insert.sql;
+\i question-insert.sql;
+\i answer-insert.sql;
+\i questionHaveAnswer-insert.sql
+\i category-insert.sql
+\i below-insert.sql
+\i permission-insert.sql
