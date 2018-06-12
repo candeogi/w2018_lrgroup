eb99ad9325d9f424ac30face50326848010c338d
luca <rossi.luca00@gmail.com>
2018-06-12 18:29:29 +0200

    restCertificate

--- src/main/java/project/rest/RestCertificate.java
+++ src/main/java/project/rest/RestCertificate.java
@@ -19,5 +19,40 @@ public class RestCertificate extends RestResource {
         super(req, res, con);
     }
 
+    public void searchCertificateByUserName() throws IOException {
+
+        List<Certificate> c = null;
+        Message m = null;
+        Message message;
+        try {
+            // parse the URI path to extract the ID
+            String path = req.getRequestURI();
+            path = path.substring(path.lastIndexOf("organization") + 2);
+
+            message = new Message(path);
+            message.toJSON(res.getOutputStream());
+
+
+            final String certificateOrganization = path.substring(1);
+
+            String IDUser = (String) req.getSession().getAttribute("loggedInUser");
+            // creates a new object for accessing the database and search the question
+            c = new SearchCertificateByUsernameDatabase(con, IDUser).SearchQuestionByUser();
+
+            if (c != null) {
+                res.setStatus(HttpServletResponse.SC_OK);
+                new ResourceList(c).toJSON(res.getOutputStream());
+            } else {
+                // it should not happen
+                m = new Message("Cannot search question: unexpected error.", "E5A1", null);
+                res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                m.toJSON(res.getOutputStream());
+            }
+        } catch (Throwable t) {
+            m = new Message("Cannot search question: unexpected error.", "E5A1", t.getMessage());
+            res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            m.toJSON(res.getOutputStream());
+        }
+    }
 
 }
